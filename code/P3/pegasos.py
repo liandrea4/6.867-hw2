3ba1a97bbf9f1279ba5a4125caabcc0ca286a6f3
Tyson Chen <ytchen33@mit.edu>
2016-10-16 23:03:11 -0400

    finished 3.1

--- code/P3/pegasos.py
+++ code/P3/pegasos.py
@@ -8,22 +8,51 @@ from plotBoundary import *
 from sklearn                import linear_model
 
 def run_pegasos(X, Y, reg_parameter, max_epochs):
-	t = 0 
-	weight = numpy.array([0.0] * len(X[0]))
-	weights_matrix = numpy.matrix([weight] * (max_epochs+1))
+	t = 0
+	epoch = 0
+	weights = numpy.array([0.0] * len(X[0]))
+	weights.reshape(2,1)
+	weights_matrix = [weights] * (max_epochs * len(X)+2)
+
+	print len(X)
+	
+	
 	step_size = 0
-	while (t < max_epochs):
+	while (epoch < max_epochs):
+		epoch +=1
+		print"pre for loop t", t
 		for i in range(len(X)):
 			t += 1
 			step_size = (1/ (t * reg_parameter))
-			inner_product = numpy.dot(numpy.transpose(weights_matrix[t]), X[i])
+
+
+			inner_product = numpy.dot(weights_matrix[t].reshape(1,2), X[i].reshape(2,1))
+
 			if numpy.dot(Y[i], inner_product) < 1:
-				weights_matrix[t+1] = (1 - step_size*reg_parameter) * weights_matrix[t] + numpy.dot(numpy.dot(step_size, Y[i]), X[i])
+	
+				a = numpy.dot((1 - step_size*reg_parameter), weights_matrix[t].reshape(2,1))
+				constant = step_size * Y[i]
+				b = numpy.dot(constant[0], X[i].reshape(2,1))
+		
+				print "in for loop t", t
+				weights_matrix[t+1] = a+b
 			else:
-				weights_matrix[t+1] = (1 - step_size*reg_parameter) * weights_matrix[t]
+				print "in for loop t", t
+
+				weights_matrix[t+1] = numpy.dot((1 - step_size*reg_parameter) , weights_matrix[t])
 
 
-	return weights_matrix
+	return weights_matrix[-1]
 
 
+if __name__ == '__main__':
 
+	train = loadtxt('../data/data3_train.csv')
+	X = train[:,0:2]
+	Y = train[:,2:3]
+
+	epochs = 4;
+	lmbda = .02;
+
+
+	print run_pegasos(X, Y, lmbda, epochs)
